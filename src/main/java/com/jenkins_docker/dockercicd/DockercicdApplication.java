64ace931c1251c2955d8277179b2a1fd827e3490
PiyushDixit12 <dixitp034@gmail.com>
2024-09-05 14:47:04 +0530

    second commit

---
+++ src/main/java/com/jenkins_docker/dockercicd/DockercicdApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins_docker.dockercicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockercicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockercicdApplication.class, args);
+	}
+
+}
