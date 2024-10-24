09ca6daa92719d96dd52d2f85cde31a7dd190eb3
Piyush Dixit <139225587+PiyushDixit12@users.noreply.github.com>
2024-10-24 12:30:37 +0530

    build success

--- src/test/java/com/jenkins_docker/dockercicd/DockercicdApplicationTests.java
+++ src/test/java/com/jenkins_docker/dockercicd/DockercicdApplicationTests.java
@@ -2,7 +2,7 @@ package com.jenkins_docker.dockercicd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-// import org.slf4j.*;
+import org.slf4j.*;
 @SpringBootTest
 class DockercicdApplicationTests {
 
